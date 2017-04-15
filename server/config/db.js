85e1fec48e2f3fbc06b2af6d80761c3ac2283d68
Pudding4ever <dkerrcresswell@gmail.com>
2017-04-15 04:52:51 -0400

    ch-ch-changes

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb://thomas:123456@ds161059.mlab.com:61059/books308"
+  //Remote MongoDB deployment -> let URI = "mongodb://thomas:123456@ds161059.mlab.com:61059/books308";
+  //My own Database for testing: "URI": "mongodb://test:swordfish@ds145299.mlab.com:45299/testdbformidterm"
+};
